29e5617f6f62719dedd13b60d9174c98531ccc0c
sravanthi palchuri <sravanthipalchuri@gmail.com>
2026-06-15 22:47:16 +0530

    Third version of dvc

--- code.py
+++ code.py
@@ -13,6 +13,10 @@ new_row_loc={'Name':'GF1','Age':20,'city':'Bengalur'}
 
 df.loc[len(df.index)]=new_row_loc
 
+new_row_loc2={'Name':'GF2','Age':28,'city':'Chennai'}
+
+df.loc[len(df.index)]=new_row_loc2
+
 data_dir='data'
 
 os.makedirs(data_dir,exist_ok=True)
